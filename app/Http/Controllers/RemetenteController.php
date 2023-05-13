edccf19c4c2cc3ead2af95fd143a8edfd89a365a
Eduardo Henrique <eduardo.hor@outlook.com>
2023-05-13 19:46:51 -0300

    :sparkles: feat/Agrupando as notas por remetente

--- app/Http/Controllers/RemetenteController.php
+++ app/Http/Controllers/RemetenteController.php
@@ -3,19 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Facades\ApiNotasFiscais;
-use Illuminate\Support\Facades\Http;
 
 class RemetenteController extends Controller
 {
-    
+
     public function index()
     {
-        $apiNotasFiscais = ApiNotasFiscais::get('api/ps/notas')->json();
+        $notasFiscais = ApiNotasFiscais::get('api/ps/notas')->json();
 
-        $cnpjRemetentes = array_unique(array_column($apiNotasFiscais, 'cnpj_remete'));
+        $remetentesCollection = collect($notasFiscais);
 
-        $test = array_fill_keys($cnpjRemetentes, $apiNotasFiscais);
-  
-        return response()->json($test, 200);
+        $notasRemententes = $remetentesCollection->groupBy('cnpj_remete');
+
+        return response()->json($notasRemententes, 200);
     }
 }
